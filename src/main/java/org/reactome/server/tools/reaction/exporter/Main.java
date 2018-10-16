a696f16de69727fb0ac60160fa585d045f8497a5
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-10-16 16:53:25 +0100

    package refactoring plus initial playground code

---
+++ src/main/java/org/reactome/server/tools/reaction/exporter/Main.java
@@ -0,0 +1,58 @@
+
+package org.reactome.server.tools.reaction.exporter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.martiansoftware.jsap.*;
+import org.reactome.server.graph.domain.model.ReactionLikeEvent;
+import org.reactome.server.graph.service.AdvancedDatabaseObjectService;
+import org.reactome.server.graph.service.DatabaseObjectService;
+import org.reactome.server.graph.utils.ReactomeGraphCore;
+import org.reactome.server.tools.reaction.exporter.config.ReactomeNeo4jConfig;
+import org.reactome.server.tools.reaction.exporter.layout.LayoutFactory;
+import org.reactome.server.tools.reaction.exporter.layout.model.Layout;
+
+/**
+ * @author Antonio Fabregat (fabregat@ebi.ac.uk)
+ * @author Pascual Lorente (plorente@ebi.ac.uk)
+ */
+public class Main {
+
+    public static void main(String[] args) throws JSAPException {
+
+        // Program Arguments -h, -p, -u, -k
+        SimpleJSAP jsap = new SimpleJSAP(Main.class.getName(), "Connect to Reactome Graph Database",
+                new Parameter[]{
+                        new FlaggedOption("host",     JSAP.STRING_PARSER, "localhost", JSAP.NOT_REQUIRED, 'h', "host",     "The neo4j host"),
+                        new FlaggedOption("port",     JSAP.STRING_PARSER, "7474",      JSAP.NOT_REQUIRED, 'p', "port",     "The neo4j port"),
+                        new FlaggedOption("user",     JSAP.STRING_PARSER, "neo4j",     JSAP.NOT_REQUIRED, 'u', "user",     "The neo4j user"),
+                        new FlaggedOption("password", JSAP.STRING_PARSER, "neo4j",     JSAP.REQUIRED,     'd', "password", "The neo4j password")
+                }
+        );
+
+        JSAPResult config = jsap.parse(args);
+        if (jsap.messagePrinted()) System.exit(1);
+
+        //Initialising ReactomeCore Neo4j configuration
+        ReactomeGraphCore.initialise(config.getString("host"), config.getString("port"), config.getString("user"), config.getString("password"), ReactomeNeo4jConfig.class);
+
+        // Access the data using our service layer.
+        DatabaseObjectService dos = ReactomeGraphCore.getService(DatabaseObjectService.class);
+        AdvancedDatabaseObjectService ads = ReactomeGraphCore.getService(AdvancedDatabaseObjectService.class);
+
+        //ReactionLikeEvent rle = dos.findById("R-HSA-6791223");
+        ReactionLikeEvent rle = dos.findById("R-HSA-211734");
+
+        Layout rxn = LayoutFactory.getReactionLikeEventLayout(ads, rle);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String json = mapper.writeValueAsString(rxn);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+    }
+}
