e4730a823ad3b44a0ba9f8aa8081e67129e15622
pasculorente <pasculorente@gmail.com>
2018-10-17 15:21:45 +0100

    Added quality to image

--- src/main/java/org/reactome/server/tools/reaction/exporter/renderer/LayoutRenderer.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/renderer/LayoutRenderer.java
@@ -11,26 +11,39 @@ import java.awt.image.BufferedImage;
 
 public class LayoutRenderer {
 
-	public BufferedImage render(RenderArgs args, Layout layout) {
-		final int width = (int) layout.getPosition().getWidth() + 1;
-		final int height = (int) layout.getPosition().getHeight() + 1;
-		final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		final Graphics2D graphics = image.createGraphics();
-		for (CompartmentGlyph compartment : layout.getCompartments()) {
-			graphics.setPaint(new Color(122, 122, 122, 122));
-			final Position position = compartment.getPosition();
-			graphics.fill(new Rectangle2D.Double(position.getX(), position.getY(), position.getWidth(), position.getHeight()));
-		}
+    public BufferedImage render(RenderArgs args, Layout layout) {
+        final double factor = Math.sqrt(scale(args.getQuality()));
+        final int width = (int) Math.ceil(factor * layout.getPosition().getWidth());
+        final int height = (int) Math.ceil(factor * layout.getPosition().getHeight());
+        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D graphics = image.createGraphics();
+        graphics.scale(factor, factor);
+        for (CompartmentGlyph compartment : layout.getCompartments()) {
+            graphics.setPaint(new Color(122, 122, 122, 122));
+            final Position position = compartment.getPosition();
+            graphics.fill(new Rectangle2D.Double(position.getX(), position.getY(), position.getWidth(), position.getHeight()));
+        }
 
-		for (EntityGlyph entity : layout.getEntities()) {
-			final Position position = entity.getPosition();
-			final Rectangle2D.Double rect = new Rectangle2D.Double(position.getX(), position.getY(), position.getWidth(), position.getHeight());
-			graphics.setPaint(Color.RED);
-			graphics.draw(rect);
-			graphics.setPaint(Color.GREEN);
-			graphics.fill(rect);
-		}
+        for (EntityGlyph entity : layout.getEntities()) {
+            final Position position = entity.getPosition();
+            final Rectangle2D.Double rect = new Rectangle2D.Double(position.getX(), position.getY(), position.getWidth(), position.getHeight());
+            graphics.setPaint(Color.RED);
+            graphics.draw(rect);
+            graphics.setPaint(Color.GREEN);
+            graphics.fill(rect);
+        }
+        System.out.println(layout.getEntities().size() + " entities");
 
-		return image;
-	}
+        return image;
+    }
+
+    private double scale(int quality) {
+        if (quality < 5) {
+            return interpolate(quality, 1, 5, 0.1, 1);
+        } else return interpolate(quality, 5, 10, 1, 3);
+    }
+
+    private double interpolate(double x, double min, double max, double dest_min, double dest_max) {
+        return (x - min) / (max - min) * (dest_max - dest_min) + dest_min;
+    }
 }
--- src/main/java/org/reactome/server/tools/reaction/exporter/renderer/RenderArgs.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/renderer/RenderArgs.java
@@ -2,4 +2,16 @@ package org.reactome.server.tools.reaction.exporter.renderer;
 
 public class RenderArgs {
 
+    private int quality = 5;
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public RenderArgs setQuality(int quality) {
+        if (quality < 1 || quality > 10)
+            throw new IllegalArgumentException("quality must be in the range [1-10]");
+        this.quality = quality;
+        return this;
+    }
 }
