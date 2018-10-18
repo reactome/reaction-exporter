509aeddd1a95e51c32c81e1360c08c42b2002756
pasculorente <pasculorente@gmail.com>
2018-10-18 14:54:25 +0100

    Fixed dissociation glyph

--- src/main/java/org/reactome/server/tools/reaction/exporter/renderer/glyph/reaction/DissociationReactionRenderer.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/renderer/glyph/reaction/DissociationReactionRenderer.java
@@ -1,8 +1,10 @@
 package org.reactome.server.tools.reaction.exporter.renderer.glyph.reaction;
 
 import org.reactome.server.tools.reaction.exporter.layout.model.ReactionGlyph;
+import org.reactome.server.tools.reaction.exporter.renderer.canvas.ImageCanvas;
 import org.reactome.server.tools.reaction.exporter.renderer.profile.DiagramProfile;
 import org.reactome.server.tools.reaction.exporter.renderer.utils.ShapeFactory;
+import org.reactome.server.tools.reaction.exporter.renderer.utils.StrokeStyle;
 
 import java.awt.*;
 
@@ -15,6 +17,12 @@ public class DissociationReactionRenderer extends ReactionRenderer {
 
     @Override
     protected Paint getFillColor(ReactionGlyph entity, DiagramProfile profile) {
-        return profile.getReaction().getStroke();
+        return profile.getReaction().getFill();
+    }
+
+    @Override
+    public void draw(ReactionGlyph entity, ImageCanvas canvas, DiagramProfile profile) {
+        super.draw(entity, canvas, profile);
+        canvas.getNodeBorder().add(ShapeFactory.getOval(entity.getPosition(), 3), getBorderColor(entity, profile), StrokeStyle.BORDER.getNormal());
     }
 }
