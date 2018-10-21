aa0f3028c259b494be577ec8bcde73db6219ef42
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-10-21 17:49:18 +0100

    aggregates the different factories

---
+++ src/main/java/org/reactome/server/tools/reaction/exporter/ReactionExporter.java
@@ -0,0 +1,39 @@
+package org.reactome.server.tools.reaction.exporter;
+
+import org.reactome.server.graph.domain.model.ReactionLikeEvent;
+import org.reactome.server.tools.diagram.data.graph.Graph;
+import org.reactome.server.tools.diagram.data.layout.Diagram;
+import org.reactome.server.tools.reaction.exporter.diagram.ReactionDiagramFactory;
+import org.reactome.server.tools.reaction.exporter.graph.ReactionGraphFactory;
+import org.reactome.server.tools.reaction.exporter.layout.LayoutFactory;
+import org.reactome.server.tools.reaction.exporter.layout.model.Layout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("unused")
+@Component
+public class ReactionExporter {
+
+    @Autowired
+    private LayoutFactory layoutFactory;
+
+    @Autowired
+    private ReactionGraphFactory graphFactory;
+
+    //public ReactionExporter(AdvancedDatabaseObjectService ads) {
+    //    layoutFactory = new LayoutFactory(ads);
+    //    graphFactory = new ReactionGraphFactory(ads);
+    //}
+
+    public Layout getReactionLayout(ReactionLikeEvent rle){
+        return layoutFactory.getReactionLikeEventLayout(rle);
+    }
+
+    public Diagram getReactionDiagram(Layout layout){
+        return ReactionDiagramFactory.get(layout);
+    }
+
+    public Graph getReactionGraph(ReactionLikeEvent rle, Layout layout){
+        return graphFactory.getGraph(rle, layout);
+    }
+}
