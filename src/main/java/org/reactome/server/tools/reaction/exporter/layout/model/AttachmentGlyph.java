a3d2fc2efc82d186c05385331bc65e00e2497c08
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-10-24 14:05:37 +0100

    graph-core updated to 1.1.15-SNAPSHOT

--- src/main/java/org/reactome/server/tools/reaction/exporter/layout/model/AttachmentGlyph.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/layout/model/AttachmentGlyph.java
@@ -22,24 +22,15 @@ public class AttachmentGlyph extends AbstractGlyph {
     private String schemaClass;
     private String name;
 
-    //Provided by Bijay Jassal
-    private static final Set<String> PHOSPHORYLATION = new HashSet<>(Arrays.asList("01931", "01614", "00045", "00046", "00047", "00048", "00043", "00044", "00227", "00330", "00260", "00254", "00251", "00259", "00272", "00261", "00218", "00208", "00232", "00233", "00376", "00377", "00328", "00439", "00435", "01451", "01972", "00042", "00890", "00696", "00541", "01792", "01452", "01163", "01797", "01798"));
-
     AttachmentGlyph(AbstractModifiedResidue amr) {
         super();
         dbId = amr.getDbId();
         schemaClass = amr.getSchemaClass();
-        try {
-            Method psiModMethod = amr.getClass().getMethod("getPsiMod");
-            PsiMod psiMod = (PsiMod) psiModMethod.invoke(amr);
-            name = PHOSPHORYLATION.contains(psiMod.getIdentifier()) ? "P" : "";
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            name = "";
-        }
-        //name = amr.getDisplayName().contains("-phospho-") ? "P" : "";
+        PsiMod psiMod = amr.fetchSingleValue("getPsiMod");
+        name = psiMod == null ? null : psiMod.getAbbreviation();
     }
 
-    public AttachmentGlyph(AttachmentGlyph attachement) {
+    AttachmentGlyph(AttachmentGlyph attachement) {
         super();
         name = attachement.name;
     }
