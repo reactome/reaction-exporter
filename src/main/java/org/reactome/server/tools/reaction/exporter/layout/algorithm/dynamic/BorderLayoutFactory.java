f25f3b97c9be65c1a3ec15af1a6489be68760d8f
Pascual Lorente <pasculorente@gmail.com>
2018-11-07 14:02:40 +0000

    small refactoring

--- src/main/java/org/reactome/server/tools/reaction/exporter/layout/algorithm/dynamic/BorderLayoutFactory.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/layout/algorithm/dynamic/BorderLayoutFactory.java
@@ -23,7 +23,13 @@ public class BorderLayoutFactory {
         return getBorderLayout(layout.getCompartmentRoot());
     }
 
+    @SuppressWarnings("ConstantConditions")
     private static BorderLayout getBorderLayout(CompartmentGlyph compartment) {
+        // Don't be afraid of reading this method, it places content of compartment into a new BorderLayout.
+        // In every coordinate
+        // When it
+        // encounters subcompartments, it creates sub BorderLayouts. It also creates sub BorderLayouts for every
+        // coordinate where
         // /-------+-------+-------\
         // |       |       |       |
         // |  NW   |   N   |   NE  |
@@ -39,6 +45,7 @@ public class BorderLayoutFactory {
         // \-------+-------+-------/
         final BorderLayout borderLayout = new BorderLayout();
         borderLayout.setCompartment(compartment);
+
         for (Glyph glyph : compartment.getContainedGlyphs())
             if (glyph instanceof ReactionGlyph) borderLayout.add(CENTER, glyph);
             else if (glyph instanceof EntityGlyph) {
@@ -69,83 +76,88 @@ public class BorderLayoutFactory {
         for (CompartmentGlyph child : compartment.getChildren()) layouts.add(getBorderLayout(child));
 
         for (BorderLayout child : layouts) {
-            boolean n = false;
-            boolean s = false;
-            boolean e = false;
-            boolean w = false;
-            boolean c = false;
-            if (!child.getGlyphs().isEmpty()) {
-                for (Glyph glyph : child.getGlyphs()) {
-                    if (glyph instanceof ReactionGlyph) c = true;
-                    else if (glyph instanceof EntityGlyph) {
-                        final EntityGlyph entityGlyph = (EntityGlyph) glyph;
-                        for (Role role : entityGlyph.getRoles()) {
-                            switch (role.getType()) {
-                                case INPUT:
-                                    w = true;
-                                    break;
-                                case OUTPUT:
-                                    e = true;
-                                    break;
-                                case CATALYST:
-                                    n = true;
-                                    break;
-                                case NEGATIVE_REGULATOR:
-                                case POSITIVE_REGULATOR:
-                                    s = true;
-                                    break;
-                            }
+            placeSubBorderLayout(borderLayout, child);
+        }
+        return borderLayout;
+    }
+
+    private static void placeSubBorderLayout(BorderLayout borderLayout, BorderLayout child) {
+        boolean n = false;
+        boolean s = false;
+        boolean e = false;
+        boolean w = false;
+        boolean c = false;
+        if (!child.getGlyphs().isEmpty()) {
+            for (Glyph glyph : child.getGlyphs()) {
+                if (glyph instanceof ReactionGlyph) c = true;
+                else if (glyph instanceof EntityGlyph) {
+                    final EntityGlyph entityGlyph = (EntityGlyph) glyph;
+                    for (Role role : entityGlyph.getRoles()) {
+                        switch (role.getType()) {
+                            case INPUT:
+                                w = true;
+                                break;
+                            case OUTPUT:
+                                e = true;
+                                break;
+                            case CATALYST:
+                                n = true;
+                                break;
+                            case NEGATIVE_REGULATOR:
+                            case POSITIVE_REGULATOR:
+                                s = true;
+                                break;
                         }
                     }
                 }
-            } else {
-                n = child.get(NORTH) != null;
-                s = child.get(SOUTH) != null;
-                e = child.get(EAST) != null;
-                w = child.get(WEST) != null;
-                c = child.get(CENTER) != null;
-                if (child.get(NORTH_EAST) != null) n = e = true;
-                if (child.get(NORTH_WEST) != null) n = w = true;
-                if (child.get(SOUTH_EAST) != null) s = e = true;
-                if (child.get(SOUTH_WEST) != null) s = w = true;
             }
-
-            // MEGA SUPER KARNAUGH MAP (4 inputs -> 8 outputs)
-            // n s e w
-            // 0 0 0 0 error
-            if (c) setOrMerge(borderLayout, CENTER, child);
-            else if (!n && !s && !e && !w) System.err.println("Whaaat");
-                // 1 1 1 1 c
-            else if (n && s && e && w) setOrMerge(borderLayout, CENTER, child);
-                // 0 1 0 0 s
-            else if (!n && s && !e && !w) setOrMerge(borderLayout, SOUTH, child);
-                // 0 1 1 1 s
-            else if (!n && s && e && w) setOrMerge(borderLayout, SOUTH, child);
-                // 1 0 1 1 n
-            else if (n && !s && e && w) setOrMerge(borderLayout, NORTH, child);
-                // 1 0 0 0 n
-            else if (n && !s && !e && !w) setOrMerge(borderLayout, NORTH, child);
-                // 0 0 0 1 w
-            else if (!n && !s && !e && w) setOrMerge(borderLayout, WEST, child);
-                // 1 1 0 1 w
-            else if (n && s && !e && w) setOrMerge(borderLayout, WEST, child);
-                // 1 1 1 0 e
-                // 0 0 1 0 e
-            else if ((n && s || !n && !s) && e && !w) setOrMerge(borderLayout, EAST, child);
-                // 0 1 0 1 sw
-            else if (!n && s && !e && w) setOrMerge(borderLayout, SOUTH_WEST, child);
-                // 1 0 0 1 nw
-            else if (n && !s && !e && w) setOrMerge(borderLayout, NORTH_WEST, child);
-                // 1 0 1 0 ne
-            else if (n && !s && e && !w) setOrMerge(borderLayout, NORTH_EAST, child);
-                // 0 1 1 0 se
-            else if (!n && s && e && !w) setOrMerge(borderLayout, SOUTH_EAST, child);
-                // 1 1 0 0 e (or w)
-            else if (n && s && !e && !w) setOrMerge(borderLayout, EAST, child);
-                // 0 0 1 1 n (or s)
-            else if (!n && !s && e && w) setOrMerge(borderLayout, NORTH, child);
+        } else {
+            n = child.get(NORTH) != null;
+            s = child.get(SOUTH) != null;
+            e = child.get(EAST) != null;
+            w = child.get(WEST) != null;
+            c = child.get(CENTER) != null;
+            if (child.get(NORTH_EAST) != null) n = e = true;
+            if (child.get(NORTH_WEST) != null) n = w = true;
+            if (child.get(SOUTH_EAST) != null) s = e = true;
+            if (child.get(SOUTH_WEST) != null) s = w = true;
         }
-        return borderLayout;
+
+        // MEGA SUPER KARNAUGH MAP (4 inputs -> 8 outputs)
+        // n s e w
+        // 0 0 0 0 error
+        if (c) setOrMerge(borderLayout, CENTER, child);
+        else if (!n && !s && !e && !w) System.err.println("Whaaat");
+            // 1 1 1 1 c
+        else if (n && s && e && w) setOrMerge(borderLayout, CENTER, child);
+            // 0 1 0 0 s
+        else if (!n && s && !e && !w) setOrMerge(borderLayout, SOUTH, child);
+            // 0 1 1 1 s
+        else if (!n && s && e && w) setOrMerge(borderLayout, SOUTH, child);
+            // 1 0 1 1 n
+        else if (n && !s && e && w) setOrMerge(borderLayout, NORTH, child);
+            // 1 0 0 0 n
+        else if (n && !s && !e && !w) setOrMerge(borderLayout, NORTH, child);
+            // 0 0 0 1 w
+        else if (!n && !s && !e && w) setOrMerge(borderLayout, WEST, child);
+            // 1 1 0 1 w
+        else if (n && s && !e && w) setOrMerge(borderLayout, WEST, child);
+            // 1 1 1 0 e
+        else if (n && s && e && !w) setOrMerge(borderLayout, EAST, child);
+            // 0 0 1 0 e
+        else if (!n && !s && e && !w) setOrMerge(borderLayout, EAST, child);
+            // 0 1 0 1 sw
+        else if (!n && s && !e && w) setOrMerge(borderLayout, SOUTH_WEST, child);
+            // 1 0 0 1 nw
+        else if (n && !s && !e && w) setOrMerge(borderLayout, NORTH_WEST, child);
+            // 1 0 1 0 ne
+        else if (n && !s && e && !w) setOrMerge(borderLayout, NORTH_EAST, child);
+            // 0 1 1 0 se
+        else if (!n && s && e && !w) setOrMerge(borderLayout, SOUTH_EAST, child);
+            // 1 1 0 0 e (or w)
+        else if (n && s && !e && !w) setOrMerge(borderLayout, EAST, child);
+            // 0 0 1 1 n (or s)
+        else if (!n && !s && e && w) setOrMerge(borderLayout, NORTH, child);
     }
 
     /**
