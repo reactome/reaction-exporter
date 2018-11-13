023e052ab03f2e5fe048bf5f656761f13413c215
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-11-13 10:47:22 +0000

    no duplicates for dashed elements

--- src/main/java/org/reactome/server/tools/reaction/exporter/layout/LayoutFactory.java
+++ src/main/java/org/reactome/server/tools/reaction/exporter/layout/LayoutFactory.java
@@ -74,6 +74,7 @@ public class LayoutFactory {
 
 
             "OPTIONAL MATCH (nr)-[:input]->(:PhysicalEntity)<-[:normalPhysicalEntity|role*]-(efs:EntityFunctionalStatus)<-[:entityFunctionalStatus]-(rle) " +
+            "WHERE NOT (rle)-[:input]->(:PhysicalEntity)<-[:physicalEntity|role*]-(efs) " +
             "OPTIONAL MATCH (efs)-[:entityFunctionalStatus|physicalEntity*]->(pe:PhysicalEntity) " +
             "OPTIONAL MATCH (pe)-[:hasComponent|hasMember|hasCandidate*]->(d:Drug) " +
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'input'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
@@ -83,6 +84,7 @@ public class LayoutFactory {
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'input'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
 
             "OPTIONAL MATCH (nr)-[:catalystActivity|physicalEntity*]->(:PhysicalEntity)<-[:normalPhysicalEntity|role*]-(efs:EntityFunctionalStatus)<-[:entityFunctionalStatus]-(rle) " +
+            "WHERE NOT (rle)-[:catalystActivity|physicalEntity*]->(:PhysicalEntity)<-[:physicalEntity|role*]-(efs) " +
             "OPTIONAL MATCH (efs)-[:entityFunctionalStatus|physicalEntity*]->(pe:PhysicalEntity) " +
             "OPTIONAL MATCH (pe)-[:hasComponent|hasMember|hasCandidate*]->(d:Drug) " +
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'catalyst'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
@@ -92,6 +94,7 @@ public class LayoutFactory {
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'catalyst'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
 
             "OPTIONAL MATCH (nr)-[:regulatedBy]->(:NegativeRegulation)-[:regulator]->(:PhysicalEntity)<-[:normalPhysicalEntity|role*]-(efs:EntityFunctionalStatus)<-[:entityFunctionalStatus]-(rle) " +
+            "WHERE NOT (rle)-[:regulatedBy]->(:NegativeRegulation)-[:regulator]->(:PhysicalEntity)<-[:physicalEntity|role*]-(efs) " +
             "OPTIONAL MATCH (efs)-[:entityFunctionalStatus|physicalEntity*]->(pe:PhysicalEntity) " +
             "OPTIONAL MATCH (pe)-[:hasComponent|hasMember|hasCandidate*]->(d:Drug) " +
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'negative'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
@@ -101,6 +104,7 @@ public class LayoutFactory {
             "WITH rle, nr, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'negative'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
 
             "OPTIONAL MATCH (nr)-[:regulatedBy]->(:PositiveRegulation)-[:regulator]->(:PhysicalEntity)<-[:normalPhysicalEntity|role*]-(efs:EntityFunctionalStatus)<-[:entityFunctionalStatus]-(rle) " +
+            "WHERE NOT (rle)-[:regulatedBy]->(:PositiveRegulation)-[:regulator]->(:PhysicalEntity)<-[:physicalEntity|role*]-(efs) " +
             "OPTIONAL MATCH (efs)-[:entityFunctionalStatus|physicalEntity*]->(pe:PhysicalEntity) " +
             "OPTIONAL MATCH (pe)-[:hasComponent|hasMember|hasCandidate*]->(d:Drug) " +
             "WITH rle, ps + COLLECT(DISTINCT CASE pe WHEN NULL THEN NULL ELSE {physicalEntity: pe, role:{n: 1, type: 'positive'}, drug: (pe:Drug) OR NOT d IS NULL, dashed:true} END) AS ps " +
